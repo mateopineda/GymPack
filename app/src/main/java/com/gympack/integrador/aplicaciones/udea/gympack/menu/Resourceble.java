fb20ab8c54e79f10c3d323eef1fb34578ac25cc5
mateopineda <jasonpineda100@gmail.com>
2015-06-14 10:56:18 -0500

    Revert "Gympack Interfaz"
    This reverts commit 3a135e66882ee6624a538046b3ebd419f445be62.

--- app/src/main/java/com/gympack/integrador/aplicaciones/udea/gympack/menu/Resourceble.java
+++
@@ -1,9 +0,0 @@
-package com.gympack.integrador.aplicaciones.udea.gympack.menu;
-
-/**
- * Created by PINEDA on 21/05/2015.
- */
-public interface Resourceble {
-    public int getImageRes();
-    public String getName();
-}
